a7e3f1df8584a347ecb8283413c8774c8bb380b4
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-11-15 12:38:39 -0500

    update

--- index.js
+++ index.js
@@ -22,9 +22,4 @@ function sendMessage(username,message){
 	var req = new XMLHttpRequest();
 	req.open("GET","http://localhost:8000/?user="+username+"&msg="+message);
 	req.send(null);
-	/*req.addEventListener("load",function(){
-		var data = JSON.parse(req.responseText);
-		console.log(req.responseText);
-		document.getElementById("messageBoard").innerHTML += "<p>"+data.user+" : " +data.msg+"</p>";
-	})*/
 }
